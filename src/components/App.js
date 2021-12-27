c86a03d5a6f35c5bdb497f07adb8658ee7559987
Kenan Kadic <ken.kadic@outlook.com>
2021-12-26 18:15:32 -0600

    App.js updated

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+const App = () => {
+    const [count, setCount] = useState(0)
+
+
+    return (
+        <div>
+            <button onClick={() => 
+            setCount(count+1)
+            }>Increase Count</button>
+            
+            <h1>{count}</h1>
+            
+            <button onClick={() => 
+            setCount(count-1)
+            }>Decrease Count</button> 
+        </div>
+    )
+}
+
+export default App; 
